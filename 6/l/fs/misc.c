2da97c639a2083c76691f8eb0ae310fcaec65537
l-iberty <2593093242@qq.com>
2017-09-17 17:07:21 +0800

    在clock_handler()中读取CMOS RAM显示时间

---
+++ 6/l/fs/misc.c
@@ -0,0 +1,32 @@
+#include "proc.h"
+#include "fs.h"
+#include "type.h"
+#include "sysconst.h"
+#include "stdio.h"
+#include "stdlib.h"
+#include "string.h"
+
+
+/**
+ * 查找根目录下的文件
+ *
+ * @param	filename 不含路径的纯文件名
+ * @return	DIR_ENTRY::nr_inode, or (0) if not found
+ */
+int find_file(char* filename)
+{
+	int nr_inode = 0;
+	
+	/* 读入根目录 ( 1 个扇区 ) */
+	read_hd(ROOTDIR_SEC, dirent_buf, sizeof(dirent_buf));
+	
+	DIR_ENTRY* pde = (DIR_ENTRY*) dirent_buf;
+	for (int i = 0; i < NR_FILES; i++, pde++) {
+		if (!_memcmp(filename, pde->name, _strlen(filename) + 1)) {
+			nr_inode = pde->nr_inode;
+			break;
+		}
+	}
+	
+	return nr_inode;
+}
---
+++ 6/l/include/string.h
@@ -0,0 +1,9 @@
+#ifndef STRING_H
+#define STRING_H
+
+#include "type.h"
+
+size_t	_strlen(const char* s);
+void	_strcpy(char* dst, const char* src);
+
+#endif /* STRING_H */
