e455beae0fb7e6918088af9812cabda953e1e957
l-iberty <2593093242@qq.com>
2019-03-18 13:55:07 +0800

    update

--- 9/a/lib/string.c
+++ 9/a/lib/string.c
@@ -9,7 +9,7 @@ int strlen(char* str)
 
 int strcmp(char* str1, char* str2)
 {
-	while (*str1 != '\0' && *str1++ == *str2++);
+	while (*str1 != '\0' && *str1 == *str2) { str1++, str2++; }
 	
 	if (*str1 == '\0' && *str2 == '\0') { return 0; }
 	else if (*str1 > *str2) { return 1; }
